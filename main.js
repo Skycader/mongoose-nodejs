a90db1f250373fa43b3548d27db6080c16321a5f
skycader <skycader@github.com>
2022-12-29 08:48:52 +0300

    Final

--- main.js
+++ main.js
@@ -1,15 +1,75 @@
-const mongoose = require('mongoose')
-const User = require('./schmemas/User')
+const mongoose = require("mongoose");
+const User = require("./schmemas/User");
 
-mongoose.connect('mongodb://localhost/mongooseapp', 
-() => { console.log('Connected to db ')},
-() => { console.log(' Failed to connect to db ')})
+mongoose.connect(
+  "mongodb://localhost/mongooseapp",
+  () => {
+    // console.log("Connected to db ");
+  },
+  () => {
+    console.log(" Failed to connect to db ");
+  }
+);
 
-async function run() {
+async function create() {
+  try {
+   
   // const user = new User({name: "Kyle", age: 26})
   // await user.save()
-  const user = await User.create({name: "Daniel", age: 26})
-  console.log(user)
+  const user = await User.create({
+    name: "Axl",
+    age: 16,
+    email: "AXL@GOOGLE.com",
+    hobbies: ["Running","Jumping"],
+    address: {
+      street: "Main st."
+    }
+  });
+  console.log(user);
+
+  // user.createdAt = 5
+  // await user.save()
+} catch(e) {
+  console.error(e.message)
+}
 }
 
-run()
+// create()
+
+async function find() {
+  try {
+   
+  // const user = await User.findById("63ac8886d87c42bff9f8052e")
+  // const user = await User.find({name: "Daniel"})
+  // const user = await User.findOne({name: "Daniel"})
+  // const user = await User.exists({name: "Daniel"})
+  // const user = await User.where("name").equals("Daniel")
+  // const user = await User.where("age").gt(21)
+  // const user = await User.where("age").gt(21).where("name").equals("Daniel")
+  // const user = await User.where("age").gt(21).lt(68).where("name").equals("Daniel")
+  // const user = await User.where("age").gt(21).lt(68).where("name").equals("Daniel").limit(2)
+  // const user = await User.where("age").gt(21).lt(68).where("name").equals("Daniel").limit(2).select("age")
+  // const user = await User.findOne({age: {$gt: 21}})
+
+  // const user = await User.where("name").equals("Daniel")
+ 
+  // user[0].bestFriend = "63ac8d80c3c5e3697a8c2848" 
+  // await user[0].save()
+  // console.log(user);
+
+  // const user = await User.where("name").equals("Daniel").populate("bestFriend") 
+  // upper does nto work with methods
+  const user = await User.findOne({name: "Daniel"})
+  // const user = await User.findByName("Daniel")
+  // const user = await User.find().byName("Daniel")
+  console.log(user)
+  // console.log(user.namedEmail)
+  await user.save()
+//  user.sayHi()
+} catch(e) {
+  console.error(e.message)
+}
+}
+
+// create()
+find()
--- schmemas/User.js
+++ schmemas/User.js
@@ -1,8 +1,67 @@
 const mongoose = require('mongoose')
 
-const userxSchema = new mongoose.Schema({
-  name: String,
-  age: Number
+const addressSchema = new mongoose.Schema({
+  street: String,
+  city: String,
 })
 
-module.exports = mongoose.model("user",userxSchema) //collection name, schema reference
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: {
+    type: Number,
+    min: 1,
+    validate: {
+      validator: v => v % 2 === 0,
+      message: props => `${props.value} is not an even number`
+    }
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+    minLength: 10,
+  },
+  createdAt: {
+    type: Date,
+    immutable: true,
+    default: () => new Date(),
+  },
+  updatedAt: {
+    type: Date,
+    default: () => new Date(),
+  },
+  bestFriend: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "user"
+  },
+  hobbies: [String],
+  address: addressSchema,
+})
+
+userSchema.methods.sayHi = function() {
+  console.log(`Hi. My name is ${this.name}`)
+}
+
+userSchema.statics.findByName = function(name) {
+  return this.find({name: new RegExp(name,"i")})
+}
+
+userSchema.query.byName = function(name) {
+  return this.where({name: new RegExp(name, "i")})
+}
+
+userSchema.virtual("namedEmail").get(function() {
+  return `${this.name} <${this.email}>`
+})
+
+userSchema.pre('save', function(next) {
+  this.updatedAt = new Date()
+  next()
+})
+
+userSchema.post('save', function(doc,next) {
+  doc.sayHi()
+  next()
+})
+
+module.exports = mongoose.model("user",userSchema) //collection name, schema reference
